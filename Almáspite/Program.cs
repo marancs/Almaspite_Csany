15c4490547dc6c98ec2f9dcdb7e36602a6400a14
Marancsics Tamás <tamas.marancsics@gmail.com>
2024-09-04 13:52:52 +0200

    af

--- Almáspite/Program.cs
+++ Almáspite/Program.cs
@@ -10,7 +10,7 @@ namespace Almáspite
     {
         Program()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
         }
 
         static void Main(string[] args)
